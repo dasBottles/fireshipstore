e5e5254cdddf0a80e4c5531554fa32e0dfbefcc0
dasBottles <arattanasack@gmail.com>
2021-10-29 16:52:43 -0700

    add ssr user profile page

---
+++ components/Navbar.js
@@ -0,0 +1,43 @@
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+import Link from "next/dist/client/link";
+
+export default function Navbar() {
+
+    const { user, username } = useContext(UserContext)
+    
+    return (
+        <nav className='navbar'>
+            <ul>
+                <li>
+                    <Link href='/'>
+                    <button className="btn-logo">FEED</button>
+                    </Link>
+                </li>
+
+                {username && (
+                    <>
+                     <li className='push-left'>
+                         <Link href='/admin'>
+                             <button className='btn-blue'>Write Post</button>
+                            </Link> 
+                     </li>
+                     <li>
+                         <Link href='/${username}'>
+                             <img src ={user?.photoURL} />
+                            </Link>
+                     </li>
+                     </>
+                )}
+
+                {!username && (
+                <li>
+                    <Link href="/enter">
+                    <button className="btn-blue">Log in</button>
+                    </Link>
+                </li>
+                )}
+            </ul>
+        </nav>
+    )
+}
